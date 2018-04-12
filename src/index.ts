00c16b2c84de2cc937afcfed43cec1ae68f56521
Raymond <ray@firestitch.com>
2018-04-12 10:48:10 -0400

    updated peer dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './fs-chat.module';
 export * from './components/chat/fschat.component';
 export * from './components/menu/fschatmenu.component';
-export * from  './components/conversation/fschatconversation.component';
+export * from './components/conversation/fschatconversation.component';
 export * from './components/conversations/fschatconversations.component';
 export * from './components/participants/fsparticipants.component';
